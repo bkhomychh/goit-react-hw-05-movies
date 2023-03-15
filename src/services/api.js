b774ffde256fcaeb65a474e9f42dbcad7f1061ae
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-15 19:23:28 +0200

    fix deployment bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = '9e68854d69dbd1100bcb425bc2f51530';
 const RESOURCE = {
   trending: '/trending/movie/week',
