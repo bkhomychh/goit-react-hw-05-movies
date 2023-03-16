d6dbab4f1f4dce533a4cc52d5745090a8ac2cc0a
Bohdan Khomych <bod.khomych@gmail.com>
2023-03-16 16:44:23 +0200

    add redirecting to the Home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import { lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import SharedLayout from './SharedLayout';
 
 const Home = lazy(() => import('pages/Home'));
 const Movies = lazy(() => import('pages/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails'));
-const ErrorPage = lazy(() => import('pages/ErrorPage'));
 const Cast = lazy(() => import('./Cast'));
 const Reviews = lazy(() => import('./Reviews'));
 
@@ -19,7 +18,7 @@ export const App = () => {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
--- src/pages/ErrorPage.jsx
+++
@@ -1,5 +0,0 @@
-const ErrorPage = () => {
-  return <h1>Oops, there is no such page :(</h1>;
-};
-
-export default ErrorPage;
